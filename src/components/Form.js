a23d53b81b72d23b95b420ec91803e233a6f5cd3
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-14 08:13:08 +0100

    removed styling file. Finished styling on form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,5 @@
 import { Container, Flex } from "@chakra-ui/layout";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import {
   Button,
   FormControl,
@@ -8,60 +9,61 @@ import {
   Input,
   Box,
 } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 const Form = () => {
   return (
     <Container maxWidth="container.md" p="5">
-      <Flex flexDirection="column">
-        <Flex mt="2">
-          <FormControl>
-            <FormLabel>Currency 1</FormLabel>
-            <Select>
-              <option>GBP</option>
-              <option>USD</option>
-            </Select>
-            <FormHelperText>
-              Select a currency you wish to convert.
-            </FormHelperText>
-          </FormControl>
-          <FormControl ml="10">
-            <FormLabel>Amount</FormLabel>
-            <Input type="number"></Input>
-            <FormHelperText>Enter the amount.</FormHelperText>
-          </FormControl>
-        </Flex>
+      <Box bg="orange.500" p="4" borderRadius="2xl">
+        <Flex flexDirection="column">
+          <Flex mt="2">
+            <FormControl>
+              <FormLabel>Currency 1</FormLabel>
+              <Select bg="white">
+                <option>GBP</option>
+                <option>USD</option>
+              </Select>
+              <FormHelperText color="white">
+                Select a currency you wish to convert.
+              </FormHelperText>
+            </FormControl>
+            <FormControl ml="10">
+              <FormLabel>Amount</FormLabel>
+              <Input type="number" bg="white"></Input>
+              <FormHelperText color="white">Enter the amount.</FormHelperText>
+            </FormControl>
+          </Flex>
 
-        <Flex mt="2">
-          <FormControl>
-            <FormLabel>Currency 2</FormLabel>
-            <Select>
-              <option>GBP</option>
-              <option>USD</option>
-            </Select>
-            <FormHelperText>
-              Select a currency you wish to convert to.
-            </FormHelperText>
-          </FormControl>
-          <FormControl ml="10">
-            <FormLabel>Amount</FormLabel>
-            <Input type="number"></Input>
-            <FormHelperText>Enter the amount.</FormHelperText>
-          </FormControl>
-        </Flex>
+          <Flex mt="2">
+            <FormControl>
+              <FormLabel>Currency 2</FormLabel>
+              <Select bg="white">
+                <option>GBP</option>
+                <option>USD</option>
+              </Select>
+              <FormHelperText color="white">
+                Select a currency you wish to convert to.
+              </FormHelperText>
+            </FormControl>
+            <FormControl ml="10">
+              <FormLabel>Amount</FormLabel>
+              <Input type="number" bg="white"></Input>
+              <FormHelperText color="white">Enter the amount.</FormHelperText>
+            </FormControl>
+          </Flex>
 
-        <Flex justifyContent="center" mt="5">
-          <Box>
-            <Button
-              colorScheme="blue"
-              size="md"
-              rightIcon={<ArrowForwardIcon />}
-            >
-              Convert
-            </Button>
-          </Box>
+          <Flex justifyContent="center" mt="5">
+            <Box>
+              <Button
+                colorScheme="green"
+                size="md"
+                rightIcon={<ArrowForwardIcon />}
+              >
+                Convert
+              </Button>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
+      </Box>
     </Container>
   );
 };
