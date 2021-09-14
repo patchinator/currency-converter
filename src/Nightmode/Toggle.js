2c37f411b6b93b3a8c957c9d2dd25e369f91ae6e
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-14 19:46:25 +0100

    added Nightmode folder to tidy up components. Added Toggle component

---
+++ src/Nightmode/Toggle.js
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/layout";
+
+const Toggle = () => {
+  return (
+    <div>
+      <Flex align="center" justify="center" height="100vh" direction="column"></Flex>
+    </div>
+  )
+};
+
+export default Toggle;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./Nightmode/theme";
+import Toggle from "./Nightmode/Toggle";
 
 ReactDOM.render(
   <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+    <Toggle />
     <App />
   </ChakraProvider>,
   document.getElementById("root")
