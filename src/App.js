949766d306a9931ed7169ff56c85f3331b92798d
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-14 08:49:17 +0100

    finished results components. Imported to app component for display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Form from "./components/Form";
+import Results from "./components/Results";
 import { Fragment } from "react";
 
 function App() {
   return (
     <Fragment>
       <Form />
+      <Results />
     </Fragment>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,18 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Flex, Container } from "@chakra-ui/layout";
 import { Text } from "@chakra-ui/layout";
 
 const Results = () => {
   return (
-    <Box>
-      <Text>20</Text>
-    </Box>
-  )
+    <Container w="100%" p="0">
+      <Box bg="orange.500" p="3" borderRadius="2xl">
+        <Flex justifyContent="center">
+          <Box bg="white" pl="5" pr="5" pt="1" pb="1" borderRadius="md" boxShadow="md">
+            <Text>$20</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Container>
+  );
 };
 
 export default Results;
