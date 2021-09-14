a954f9699c90d3abda1248f9aca38b5f2b7b9c40
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-14 20:11:38 +0100

    added light/dark functionality. Removed Toggle file, was unnecessary

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
-import { Container, Box, Text } from "@chakra-ui/layout";
+import { Box, Text, Flex } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
 
 const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container width="100%" p="0">
-      <Box bgColor="blue.500" mb="5" textAlign="center" borderRadius="2xl">
-        <Text fontSize="xx-large" color="white" pb="1">Currency Converter</Text>
-      </Box>
-    </Container>
+    <Box bgColor="blue.500" mb="5" borderRadius="2xl" width="100%">
+      <Flex flexDirection="row" justifyContent="space-around" alignItems="center">
+        <Text fontSize="xx-large" color="white" pb="1">
+          Currency Converter
+        </Text>
+        <Button onClick={toggleColorMode}>{colorMode}</Button>
+      </Flex>
+    </Box>
   );
-  
-}
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./Nightmode/theme";
-import Toggle from "./Nightmode/Toggle";
 
 ReactDOM.render(
   <ChakraProvider>
     <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-    <Toggle />
     <App />
   </ChakraProvider>,
   document.getElementById("root")
