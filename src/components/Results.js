943685dba1d3c79a9c068085091792527cfc22c6
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-09-14 08:27:05 +0100

    created results component to display results on form submission

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+import { Text } from "@chakra-ui/layout";
+
+const Results = () => {
+  return (
+    <Box>
+      <Text>20</Text>
+    </Box>
+  )
+};
+
+export default Results;
